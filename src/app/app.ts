2354c9398b838b628492fdca78b1e20efe5c6c3e
FIXED-TERM Benko Benedek (GS/PSD232) <bbe9bp@bosch.com>
2026-09-02 14:46:47 +0200

    menu buttons added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { Nav } from "../components/nav/nav";
 import { SimpleRequest } from '../components/simple-request/simple-request';
+import { DetailedRequest } from '../components/detailed-request/detailed-request';
+import { Result } from '../components/result/result';
+import { ViewTypes } from '../Models/view-types';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav,SimpleRequest],
+  imports: [SimpleRequest,DetailedRequest,Result],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  
+  viewtype:ViewTypes=ViewTypes.simple
+  ViewTypes=ViewTypes
+
+  ChangeView(view:ViewTypes){
+    this.viewtype=view
+  }
+
 }
