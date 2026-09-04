c49f8fe3dcd955501d31ac4810eeeadf4b31ed30
Benedek Benko <bbe9bp@bosch.com>
2026-09-04 09:12:14 +0200

    popup components added

---
+++ src/components/popups/alertpopup/alertpopup.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alertpopup',
+  imports: [],
+  templateUrl: './alertpopup.html',
+  styleUrl: './alertpopup.css',
+})
+export class Alertpopup {}
---
+++ src/components/popups/successpopup/successpopup.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-successpopup',
+  imports: [],
+  templateUrl: './successpopup.html',
+  styleUrl: './successpopup.css',
+})
+export class Successpopup {}
